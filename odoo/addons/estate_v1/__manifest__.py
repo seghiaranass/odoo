45cd45d642cfa87fa1f98a6cde074eb75d3dcf5e
Anass Seghiar <anass.segh97@gmail.com>
2023-09-20 14:06:12 +0100

    vies

--- odoo/addons/estate_v1/__manifest__.py
+++ odoo/addons/estate_v1/__manifest__.py
@@ -2,6 +2,8 @@
     'name':'Real Estate V2',
     'application': True,
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ]
 }
--- odoo/addons/estate_v1/models/estate_property_v1.py
+++ odoo/addons/estate_v1/models/estate_property_v1.py
@@ -1,5 +1,5 @@
 from odoo import fields, models 
-
+from datetime import timedelta
 class EstatePropertyV1(models.Model):
     _name = "estate_property_v1"
     _description = "Test Model"
@@ -7,10 +7,10 @@ class EstatePropertyV1(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,default=lambda self: fields.date.today() + timedelta(days=90))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True,copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,3 +20,12 @@ class EstatePropertyV1(models.Model):
         string='garden_orientation',
         selection=[('north','North'),('south','South'),('east','East'),('west','West')],
         help = "garden_orientation is used to look which orientation that a garden refers to")
+    active = fields.Boolean(string='Display', default=True)
+    state = fields.Selection(
+        string= 'Status',
+        selection= [('new','New'),('offer_received','Offer Received'),('offer_accepted','Offer Accepted'),('sold','Sold'),('canceled','Canceled')],
+        help= '',
+        default = 'new'
+    )
+    
+    
